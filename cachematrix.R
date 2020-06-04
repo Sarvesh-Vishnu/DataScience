a2cb75d00709fa95a581c8b139978b52246899d3
Sarvesh Vishnu <64647928+Sarvesh-Vishnu@users.noreply.github.com>
2020-06-04 10:56:43 +0530

    Programming Assignment- 2

---
+++ cachematrix.R
@@ -0,0 +1,31 @@
+## Put comments here that give an overall description of what your
+## We are writing functions that can cache potentially time consuming copmutations. 
+
+## Following function creates an unique matrix object that can cache its inverse 
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+## The following function computes the inverse of the matrix returned by the above given function.  
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
+}
